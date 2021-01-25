eb46ca15b895c3e6368de51866b811371fe1ef72
eandalined@gmail.com <johndoe@example.com>
2021-01-25 19:42:57 +0700

    add index htm in admin lte 3

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,9 +1,11 @@
 import { ComponentsComponent } from './components/components.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
   {path: '', component: ComponentsComponent},
+  {path: 'index', component: IndexComponent},
 ];
 
 @NgModule({
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,12 @@ import { ComponentsRoutingModule } from './components-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsComponent } from './components/components.component';
+import { IndexComponent } from './index/index.component';
 
 
 
 @NgModule({
-  declarations: [ComponentsComponent],
+  declarations: [ComponentsComponent, IndexComponent],
   imports: [
     CommonModule,
     ComponentsRoutingModule
