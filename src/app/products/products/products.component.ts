3ed25ad1445fa4c1bbaab940384a540b849de7d2
Eanda <eandalined@gmail.com>
2021-01-26 23:48:27 +0700

    --- Auto-Git Commit ---

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,9 +6,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    this.products = dataItmes;
   }
 
 }
+
+interface Product {
+  productName: string;
+  price: number;
+  url: string;
+}
+const dataItmes: Product[] = [
+  {
+    productName: "Product A",
+    price: 30,
+    url: 'https://webstockreview.net/images/banana-clipart-banna-9.jpg',
+  },
+  {
+    productName: "Product B",
+    price: 30,
+    url: 'https://webstockreview.net/images/banana-clipart-banna-9.jpg',
+  },
+  {
+    productName: "Product C",
+    price: 30,
+    url: 'https://webstockreview.net/images/banana-clipart-banna-9.jpg',
+  }
+];
+
