8f0b97e69c845223760f581dc46a38755e6effd5
eandalined@gmail.com <johndoe@example.com>
2021-01-26 19:34:06 +0700

    commit

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsComponent } from './components/components.component';
 import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './header/header.component';
+import { BoxComponent } from './box/box.component';
 
 
 
 @NgModule({
-  declarations: [ComponentsComponent, IndexComponent],
+  declarations: [ComponentsComponent, IndexComponent, HeaderComponent, BoxComponent],
   imports: [
     CommonModule,
     ComponentsRoutingModule
+  ],
+  exports: [
+    HeaderComponent,
+    BoxComponent
   ]
 })
 export class ComponentsModule { }
