0c229620ff439f517bd49f3ae7196e56476c349a
Eanda <eandalined@gmail.com>
2021-01-27 00:10:44 +0700

    commit

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -9,7 +9,43 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  scroll    = "";
+
   ngOnInit(): void {
+  }
+
+
+
+  ngAfterViewInit(): void {
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+    window.addEventListener('scroll', this.menuscroll, false); // third parameter
+  }
+
+  @HostListener('window:storage', ['$event'])
+
+  menuscroll() {
+    if ($(".menuWrap").length > 0 && $(".page_title .guideWrap").length > 0) {
+      const sc    = $(window).scrollTop();
+      console.log(sc);
+      // const menuH = $(".menuWrap").offset().top;
+      // const chat  = $(".page_title .guideWrap").offset().top;
+
+      // if (sc >= menuH) {
+      //   $(".menuWrap").addClass("fix");
+      // } else {
+      //   $(".menuWrap").removeClass("fix");
+      // }
+
+      // if (sc + 56 >= chat) {
+      //   $(".btn_livechat").addClass("fix");
+      //   $(".btn_top").show();
+      // } else {
+      //   $(".btn_livechat").removeClass("fix");
+      //   $(".btn_top").hide();
+      // }
+    }
   }
 
 }
--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,13 +8,44 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent implements OnInit {
 
   products: Product[] = [];
-
+  scroll    = "";
   constructor() {
 
   }
 
   ngOnInit(): void {
     this.products = dataItmes;
+  }
+
+  ngAfterViewInit(): void {
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+    window.addEventListener('scroll', this.menuscroll, false); // third parameter
+  }
+
+  @HostListener('window:storage', ['$event'])
+
+  menuscroll() {
+    if ($(".menuWrap").length > 0 && $(".page_title .guideWrap").length > 0) {
+      const sc    = $(window).scrollTop();
+      console.log(sc);
+      // const menuH = $(".menuWrap").offset().top;
+      // const chat  = $(".page_title .guideWrap").offset().top;
+
+      // if (sc >= menuH) {
+      //   $(".menuWrap").addClass("fix");
+      // } else {
+      //   $(".menuWrap").removeClass("fix");
+      // }
+
+      // if (sc + 56 >= chat) {
+      //   $(".btn_livechat").addClass("fix");
+      //   $(".btn_top").show();
+      // } else {
+      //   $(".btn_livechat").removeClass("fix");
+      //   $(".btn_top").hide();
+      // }
+    }
   }
 
 }
@@ -36,6 +68,20 @@ const dataItmes: Product[] = [
   },
   {
     productName: "Product C",
+    price: 30,
+    url: 'https://webstockreview.net/images/banana-clipart-banna-9.jpg',
+  },
+  {
+    productName: "Product D",
+    price: 30,
+    url: 'https://scontent-sin6-3.xx.fbcdn.net/v/t1.0-9/1934418_123296467111_5934916_n.jpg?_nc_cat=110&ccb=2&_nc_sid=cdbe9c&_nc_eui2=AeFnSRuGzPwYRNGXnmoGHW9RTz2Qo18Fs7tPPZCjXwWzuyoMvyTlpaZdl9pe3UiJWKO2CFE18FD0nF9oMkm2UDh4&_nc_ohc=Z91p7e6m08UAX9zLzQE&_nc_ht=scontent-sin6-3.xx&oh=e6957aa6c27f6dc39b39b18cdccb830b&oe=6034E0D3',
+  },
+  {
+    productName: "Product E",
+    price: 30,
+    url: 'https://webstockreview.net/images/banana-clipart-banna-9.jpg',
+  },{
+    productName: "Product F",
     price: 30,
     url: 'https://webstockreview.net/images/banana-clipart-banna-9.jpg',
   }
